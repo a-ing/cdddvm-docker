c4a6be0d1b0ec2dd9aefb7425d374f61545ae32a
jma78 <jma78@emory.edu>
2017-05-21 20:29:28 -0400

    update network
    update network

--- train-cnn.py
+++ train-cnn.py
@@ -1,9 +1,12 @@
+import os
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import keras
+import json
 from keras.layers import Conv3D, MaxPool3D, Flatten, Dense, Dropout, Input, BatchNormalization
 from keras.models import Model
+
 
 # load the data
 with h5py.File('full_dataset_vectors.h5', 'r') as hf:
@@ -36,13 +39,13 @@ y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
 # get the cnn model
-def get_model():
+def get_model(weights_path=None):
     ins = Input((16, 16, 16, 3))
     con1 = Conv3D(filters=8, kernel_size=(3, 3, 3), activation='relu')(ins)
     con2 = Conv3D(filters=16, kernel_size=(3, 3, 3), activation='relu')(con1)
     maxp3 = MaxPool3D(pool_size=(2, 2, 2))(con2)
     con4 = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu')(maxp3)
-    con5 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu')(con4)
+    con5 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='sigmoid')(con4)
     maxp6 = MaxPool3D(pool_size=(2, 2, 2))(con2)
     batch = BatchNormalization()(maxp6)
     flat = Flatten()(batch)
@@ -52,11 +55,24 @@ def get_model():
     drop2 = Dropout(0.7)(dens2)
     outs = Dense(units=10, activation='softmax')(drop2)
     model = Model(inputs=ins, outputs=outs)
-    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(lr=0.1), metrics=['accuracy'])
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(lr=0.01), metrics=['accuracy'])
+    
+    if weights_path:
+        model.load_weights(weights_path)
     
     return model
 
 model = get_model()
 
 # train our model
-model.fit(x=xtrain[:8000], y=y_train[:8000], batch_size=10, epochs=50, validation_split=0.2)
+model.fit(x=xtrain[:20], y=y_train[:20], batch_size=5, epochs=10, validation_split=0.2)
+model.evaluate(xtest, y_test, verbose=0)
+model.save_weights('saved_weights.h5')
+model = get_model('saved_weights.h5')
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer=sgd, loss='categorical_crossentropy')
+out=model.predict(xtest)
+out=pd.DataFrame(out)
+out.to_csv('result.csv')
+
+
